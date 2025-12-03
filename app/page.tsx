3873dcb216ec44a3512d9d6b010b0f42438f0006
Climbingsyl <climbingsyl@hotmail.com>
2025-12-03 18:41:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Climbing Syl</h1>
-      <p>Bienvenue sur le site de réservation officiel.</p>
-    </main>
-  );
+export default function Page() {
+  return <h1>Hello world</h1>;
 }
